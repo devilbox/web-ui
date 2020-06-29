e8bf09f53929774d8fbd0148ce02d8855c06a042
Tobias Winkler <info@tobiaswinkler.berlin>
2020-06-29 02:14:32 +0200

    added dark theme
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,33 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import { GlobalStyle } from 'styles/global-styles';
 
 import { HomePage } from './containers/HomePage/Loadable';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
 
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Helmet titleTemplate="%s - Devilbox" defaultTitle="Devilbox">
-        <meta name="description" content="The web UI for Devilbox" />
-      </Helmet>
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Helmet titleTemplate="%s &ndash; Devilbox" defaultTitle="Devilbox">
+          <meta name="description" content="The web UI for Devilbox" />
+        </Helmet>
 
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-      <GlobalStyle />
-    </BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
