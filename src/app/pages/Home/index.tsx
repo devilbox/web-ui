db6bf022927bad918b7a72d7a13e27f2d714c5c3
Tobias Winkler <info@tobiaswinkler.berlin>
2020-06-30 13:41:02 +0200

    added logo
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Container, Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
-const Home = () => (
-  <>
-    <Helmet>
-      <title>Home</title>
-      <meta name="description" content="Devilbox Web-UI" />
-    </Helmet>
-    <Container>
-      <Typography variant="h1" component="h2" gutterBottom>
-        Home
-      </Typography>
-
-      <Typography variant="body1" gutterBottom>
-        Content
-      </Typography>
-    </Container>
-  </>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    logo: {
+      maxWidth: '100%',
+      margin: theme.spacing(4, 0),
+    },
+  }),
 );
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Helmet>
+        <title>Home</title>
+        <meta name="description" content="Devilbox Web-UI" />
+      </Helmet>
+      <Container>
+        <img
+          src={`${process.env.PUBLIC_URL}/logo/long/banner_512_trans.png`}
+          alt="devilbox logo"
+          className={classes.logo}
+        />
+
+        <Typography variant="body1" gutterBottom>
+          Content
+        </Typography>
+      </Container>
+    </>
+  );
+};
 
 export default Home;
