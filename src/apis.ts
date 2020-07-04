82fc57a3dabea9edf1296504401cdbc58baa8606
Tobias Winkler <info@tobiaswinkler.berlin>
2020-07-04 20:44:28 +0200

    added mocks for vhosts
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/apis.ts
+++ src/apis.ts
@@ -1,22 +1,34 @@
 const isProduction = process.env.NODE_ENV === 'production';
 const mockPath = `${process.env.PUBLIC_URL}/__mocks__/apiRequests`;
 
-const getAppData = isProduction
+export const getAppData = isProduction
   ? 'localhost/getAppData'
   : `${mockPath}/getAppData.json`;
 
-const getDockerData = isProduction
+export const getDockerData = isProduction
   ? 'localhost/getDockerData'
   : `${mockPath}/getDockerData.json`;
 
-const makeGetContainerData = (id: string) =>
+export const makeGetContainerData = (id: string) =>
   isProduction
     ? `localhost/getContainerData/${id}`
     : `${mockPath}/containerData/${id}.json`;
 
-const makeGetToolsData = (id: string) =>
+export const makeGetToolsData = (id: string) =>
   isProduction
     ? `localhost/getToolsData/${id}`
     : `${mockPath}/tools/${id}.json`;
 
-export { getAppData, getDockerData, makeGetContainerData, makeGetToolsData };
+export const getVhosts = isProduction
+  ? 'localhost/getVhosts'
+  : `${mockPath}/getVhosts.json`;
+
+export const makeGetVhostDirectory = (id: string) =>
+  isProduction
+    ? `localhost/vhosts/directory/${id}`
+    : `${mockPath}/vhosts/directory/${id}.json`;
+
+export const makeGetVhostsDomain = (id: string) =>
+  isProduction
+    ? `localhost/vhosts/domain/${id}`
+    : `${mockPath}/vhosts/domain/${id}.json`;
